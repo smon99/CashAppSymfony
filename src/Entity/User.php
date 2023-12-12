8665dfd38fac5c651c538902c544bd59182ecdfa
Simon de Wendt <simon.dewendt@cec.valantic.com>
2023-12-12 11:29:57 +0100

    implement security bundle and make user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,20 +1,18 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
-    #[ORM\Column]
-    private ?int $userID = null;
 
     #[ORM\Column(length: 15)]
     private ?string $username = null;
@@ -25,21 +23,12 @@ class User
     #[ORM\Column(length: 100)]
     private ?string $password = null;
 
+    #[ORM\Column]
+    private array $roles = [];
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUserID(): ?int
-    {
-        return $this->userID;
-    }
-
-    public function setUserID(int $userID): static
-    {
-        $this->userID = $userID;
-
-        return $this;
     }
 
     public function getUsername(): ?string
@@ -76,5 +65,25 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->username;
     }
 }
