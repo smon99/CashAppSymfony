b0a12cea3fb6969f0df0d47d56048e4ffe22ae65
Simon de Wendt <simon.dewendt@cec.valantic.com>
2024-03-20 13:28:43 +0100

    deposit controller adjustments

--- src/Component/Account/Communication/Controller/DepositController.php
+++ src/Component/Account/Communication/Controller/DepositController.php
@@ -5,66 +5,52 @@ namespace App\Component\Account\Communication\Controller;
 use App\Component\Account\Business\AccountBusinessFacade;
 use App\Component\Account\Business\Form\DepositFormType;
 use App\Component\Account\Business\Validation\Collection\AccountValidationException;
+use App\Component\User\Business\UserBusinessFacade;
 use App\DTO\TransactionValueObject;
 use App\Entity\DepositValue;
 use App\Symfony\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DepositController extends AbstractController
 {
     public function __construct(
-        private readonly AccountBusinessFacade $accountBusinessFacade
+        private readonly AccountBusinessFacade $accountBusinessFacade,
+        private readonly UserBusinessFacade    $userBusinessFacade,
     )
     {
     }
 
-    #[Route('/deposit', name: 'deposit')]
-    public function action(Request $request): Response
-    {
-        $balance = $this->accountBusinessFacade->calculateBalance($this->getLoggedInUser()->getId());
-        $error = $request->query->get('error');
-        $success = $request->query->get('success');
-
-        $depositValue = new DepositValue();
-        $form = $this->createForm(DepositFormType::class, $depositValue);
-
-        return $this->render('deposit/index.html.twig', [
-            'controller_name' => 'Deposit Controller',
-            'balance' => $balance,
-            'error' => $error,
-            'success' => $success,
-            'form' => $form->createView(),
-        ]);
-    }
-
     #[Route('/deposit/submit', name: 'deposit_submit', methods: ['POST'])]
-    public function submit(Request $request): RedirectResponse
+    public function submit(Request $request): JsonResponse
     {
+        $data = json_decode($request->getContent(), true);
+        $token = $request->headers->get('Authorization');
+        $activeUser = $this->userBusinessFacade->getUserFromToken($token);
+
         $depositValue = new DepositValue();
         $form = $this->createForm(DepositFormType::class, $depositValue);
-        $form->handleRequest($request);
+        $form->submit($data);
 
-        if ($form->isSubmitted()) {
+        if ($form->isValid()) {
             $value = $this->accountBusinessFacade->transformInput($depositValue->getValue());
-            $activeUserID = $this->getLoggedInUser()->getId();
 
             try {
-                $this->accountBusinessFacade->validate($value, $activeUserID);
+                $this->accountBusinessFacade->validate($value, $activeUser->getId());
             } catch (AccountValidationException $e) {
-                $error = $e->getMessage();
-                return $this->redirectToRoute('deposit', ['error' => $error]);
+                return new JsonResponse([
+                    'error' => $e,
+                ]);
             }
+
+            $transactionValueObject = new TransactionValueObject(
+                value: $value, userId: $activeUser->getId()
+            );
+            $this->accountBusinessFacade->saveDeposit($transactionValueObject);
         }
-
-        $transactionValueObject = new TransactionValueObject(
-            value: $value, userId: $activeUserID
-        );
-        $this->accountBusinessFacade->saveDeposit($transactionValueObject);
-
-        $success = 'Die Transaktion wurde erfolgreich gespeichert!';
-        return $this->redirectToRoute('deposit', ['success' => $success]);
+        return new JsonResponse([
+            'success' => 'Die Transaktion wurde erfolgreich gespeichert!',
+        ]);
     }
 }
